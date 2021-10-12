c479731f58a3462a05c7de837d9c5d069e15b82a
Novita <novitaguok@github.com>
2021-10-12 19:58:30 +0700

    Resolve all reviews for home feature

--- app/src/main/java/com/example/ihonboarding/screens/ScreenHome.kt
+++ app/src/main/java/com/example/ihonboarding/screens/ScreenHome.kt
@@ -9,28 +9,17 @@ import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.ihonboarding.R
 import com.example.ihonboarding.components.CardTopic
 import com.example.ihonboarding.components.Profile
+import com.example.ihonboarding.ui.theme.DarkJungleGreen
 import com.example.ihonboarding.ui.theme.IHOnboardingTheme
-
-class ScreenHome : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            IHOnboardingTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.secondary) {
-                    HomePage()
-                }
-            }
-        }
-    }
-}
 
 @Composable
 fun HomePage() {
@@ -38,9 +27,9 @@ fun HomePage() {
         item {
             Column() {
                 Profile(
-                    "Novita",
-                    "It's never too old to learn",
-                    "https://www.linkedin.com/in/novitaguok/"
+                    stringResource(R.string.my_name),
+                    stringResource(R.string.my_motto),
+                    stringResource(R.string.my_url)
                 )
                 Spacer(
                     modifier = Modifier
@@ -54,7 +43,7 @@ fun HomePage() {
                     )
                     Text(
                         text = "See What’s Happening in the World",
-                        style = TextStyle(color = Color(0xFF222222))
+                        style = TextStyle(color = DarkJungleGreen)
                     )
                 }
                 Spacer(modifier = Modifier.height(16.dp))
@@ -63,8 +52,8 @@ fun HomePage() {
         item {
             Column(modifier = Modifier.padding(20.dp, 0.dp)) {
                 CardTopic(
-                    title = "John Stones Reborn When City Get Rid of Manchester United",
-                    publishTime = "7 Jan 21"
+                    title = stringResource(R.string.title_card),
+                    publishTime = stringResource(R.string.time_card)
                 )
             }
         }
@@ -73,7 +62,7 @@ fun HomePage() {
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview4() {
+fun HomePagePreview() {
     IHOnboardingTheme {
         Surface(color = MaterialTheme.colors.secondary) {
             HomePage()
