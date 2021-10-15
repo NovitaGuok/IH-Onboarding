189adf8e01709ba8d57dbbfb89ff65020553bfb8
Novita <novitaguok@github.com>
2021-10-15 23:21:04 +0700

    Fixed home screen UI

--- app/src/main/java/com/example/ihonboarding/MainActivity.kt
+++ app/src/main/java/com/example/ihonboarding/MainActivity.kt
@@ -8,7 +8,7 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.ihonboarding.ui.theme.IHOnboardingTheme
+import com.example.ihonboarding.fdsa.theme.IHOnboardingTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/example/ihonboarding/ui/theme/Color.kt
@@ -0,0 +1,12 @@
+package com.example.ihonboarding.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
+val BalticSea = Color(0xFF282828)
+val Mercury = Color(0xFFE5E5E5)
+val Monsoon = Color(0xFF888888)
+val DarkJungleGreen = Color(0xFF222222)
