e8cca46d09024882e2a64eb712677d777755f17a
Novita <novitaguok@github.com>
2021-10-19 03:37:50 +0700

    Created domain model

---
+++ app/src/main/java/com/example/ihonboarding/domain/model/Profile.kt
@@ -0,0 +1,12 @@
+package com.example.ihonboarding.domain.model
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Profile(
+    var bio: String? = null,
+    var name: String? = null,
+    var picture: String? = null,
+    var username: String? = null,
+    var web: String? = null
+)
