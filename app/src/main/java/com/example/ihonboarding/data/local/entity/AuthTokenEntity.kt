f76ecea6b57967edf0d83a2bae5b9d7a52a724cd
Novita <novitaguok@github.com>
2021-10-26 05:05:33 +0700

    Changed email as PK

--- app/src/main/java/com/example/ihonboarding/data/local/entity/AuthTokenEntity.kt
+++ app/src/main/java/com/example/ihonboarding/data/local/entity/AuthTokenEntity.kt
@@ -7,12 +7,9 @@ import androidx.room.PrimaryKey
 @Entity(tableName = "auth_token")
 data class AuthTokenEntity (
     @PrimaryKey(autoGenerate = true)
-    @ColumnInfo(name = "id")
-    var id: Int? = 0,
+    @ColumnInfo(name = "email")
+    var email: String? = null,
 
     @ColumnInfo(name = "token")
     var token: String? = null,
-
-    @ColumnInfo(name = "email")
-    var email: String? = null,
 )
