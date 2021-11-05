f1503728cfa9f72f76d2bba8fb0128499fd4bff6
NovitaGuok <novitaguok@gmail.com>
2021-11-05 16:54:45 +0700

    Updated usecase

--- app/src/main/java/com/example/ihonboarding/domain/home/use_case/GetNewsUseCase.kt
+++ app/src/main/java/com/example/ihonboarding/domain/home/use_case/GetNewsUseCase.kt
@@ -2,7 +2,8 @@ package com.example.ihonboarding.domain.home.use_case
 
 import com.example.ihonboarding.data.home.model.News
 import com.example.ihonboarding.domain.home.repository.NewsRepository
+import javax.inject.Inject
 
-class GetNewsUseCase(private val newsRepository: NewsRepository) {
+class GetNewsUseCase @Inject constructor(private val newsRepository: NewsRepository) {
     suspend fun execute(): List<News>? = newsRepository.getNews()
 }
--- app/src/main/java/com/example/ihonboarding/domain/home/use_case/UpdateNewsUseCase.kt
+++ app/src/main/java/com/example/ihonboarding/domain/home/use_case/UpdateNewsUseCase.kt
@@ -2,7 +2,8 @@ package com.example.ihonboarding.domain.home.use_case
 
 import com.example.ihonboarding.data.home.model.News
 import com.example.ihonboarding.domain.home.repository.NewsRepository
+import javax.inject.Inject
 
-class UpdateNewsUseCase(private val newsRepository: NewsRepository) {
+class UpdateNewsUseCase @Inject constructor(private val newsRepository: NewsRepository) {
     suspend fun execute(): List<News>? = newsRepository.updateNews()
 }
