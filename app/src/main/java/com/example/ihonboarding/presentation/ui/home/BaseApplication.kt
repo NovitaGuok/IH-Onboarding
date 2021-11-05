265d8b9f8ad1afc0209b720f5d43100cb823eb05
NovitaGuok <novitaguok@gmail.com>
2021-11-05 16:57:20 +0700

    Deleted unused file

--- app/src/main/java/com/example/ihonboarding/presentation/ui/home/BaseApplication.kt
+++
@@ -1,8 +0,0 @@
-package com.example.ihonboarding.presentation.ui.home
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class BaseApplication : Application() {
-}
