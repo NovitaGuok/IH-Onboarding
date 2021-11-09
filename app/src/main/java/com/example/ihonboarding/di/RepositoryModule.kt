9ca4b45ba38281c082e0522100d80d2b813f85bb
novitaguok <novitaguok@gmail.com>
2021-11-10 02:27:54 +0700

    Removed Singleton annotation and changed Provide to Binds annotation

--- app/src/main/java/com/example/ihonboarding/di/RepositoryModule.kt
+++ app/src/main/java/com/example/ihonboarding/di/RepositoryModule.kt
@@ -1,22 +1,15 @@
 package com.example.ihonboarding.di
 
-import com.example.ihonboarding.data.home.data_source.local.dao.NewsDao
-import com.example.ihonboarding.data.home.data_source.remote.api.NewsService
 import com.example.ihonboarding.data.home.repository.NewsRepositoryImpl
 import com.example.ihonboarding.domain.home.repository.NewsRepository
 import dagger.Binds
 import dagger.Module
-import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-class RepositoryModule {
-    @Provides
-    @Singleton
-    fun provideNewsRepository(newsService: NewsService, newsDao: NewsDao): NewsRepository {
-        return NewsRepositoryImpl(newsService, newsDao)
-    }
+abstract class RepositoryModule {
+    @Binds
+    abstract fun provideNewsRepository(newsRepositoryImpl: NewsRepositoryImpl): NewsRepository
 }
