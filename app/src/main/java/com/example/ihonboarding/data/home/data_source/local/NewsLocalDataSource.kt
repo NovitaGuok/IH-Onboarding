66c44c2bf9856fe625ccf93358bdc2b2dab846c9
novitaguok <novitaguok@gmail.com>
2021-11-09 11:03:15 +0700

    Removed unused files first

--- app/src/main/java/com/example/ihonboarding/data/home/data_source/local/NewsLocalDataSource.kt
+++
@@ -1,9 +0,0 @@
-package com.example.ihonboarding.data.home.data_source.local
-
-import com.example.ihonboarding.data.home.model.News
-
-interface NewsLocalDataSource {
-    suspend fun getNewsFromDb(): List<News>
-    suspend fun saveNewsToDb(news: List<News>)
-    suspend fun clearAll()
-}
