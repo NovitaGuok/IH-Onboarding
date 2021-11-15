c890131494ab667db60ded1e03d37ca26a2b522b
novitaguok <novitaguok@gmail.com>
2021-11-15 13:57:37 +0700

    Some utils created

---
+++ app/src/main/java/com/example/ihonboarding/util/Constant.kt
@@ -0,0 +1,7 @@
+package com.example.ihonboarding.util
+
+const val DATABASE_NAME = "app_db"
+const val USER_PREF_NAME = "user_pref"
+const val PREVIOUS_USER_PREF_NAME = "com.example.ihonboarding.PREVIOUS_AUTH_USER_PREF"
+const val TAG_D = "AppDebug"
+const val DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ss'Z'"
