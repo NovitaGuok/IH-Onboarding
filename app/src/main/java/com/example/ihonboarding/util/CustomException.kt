742df362e82eeaabb54eaeae4483c151c09fc151
novitaguok <novitaguok@gmail.com>
2021-11-16 15:01:28 +0700

    Removed unused code

--- app/src/main/java/com/example/ihonboarding/util/CustomException.kt
+++ app/src/main/java/com/example/ihonboarding/util/CustomException.kt
@@ -1,6 +1,5 @@
 package com.example.ihonboarding.util
 
-class DysfunctionException() : Throwable("This function is not used") {}
 class NetworkException(msg: String?, errCode: ErrorCode) : Throwable("This function is not used") {
     val errCode = errCode
 }
