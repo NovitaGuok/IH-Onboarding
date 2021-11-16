294dff87192cab3629db4387161ecdb292dbf5f4
novitaguok <novitaguok@gmail.com>
2021-11-16 14:58:24 +0700

    Remove unused imports

--- app/src/main/java/com/example/ihonboarding/domain/login/use_case/LoginUseCase.kt
+++ app/src/main/java/com/example/ihonboarding/domain/login/use_case/LoginUseCase.kt
@@ -3,6 +3,7 @@ package com.example.ihonboarding.domain.login.use_case
 import com.example.ihonboarding.domain.login.repository.AuthRepository
 import com.example.ihonboarding.feature.login.data_source.remote.dto.AuthReqDto
 import javax.inject.Inject
+import javax.inject.Named
 
 class LoginUseCase @Inject constructor(private val authRepository: AuthRepository) {
     suspend fun invoke(username: String, password: String) =
