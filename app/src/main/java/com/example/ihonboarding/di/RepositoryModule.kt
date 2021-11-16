a806bb06f17d80e810242cb78963820d973f6311
novitaguok <novitaguok@gmail.com>
2021-11-16 14:58:07 +0700

    Updated repo module

--- app/src/main/java/com/example/ihonboarding/di/RepositoryModule.kt
+++ app/src/main/java/com/example/ihonboarding/di/RepositoryModule.kt
@@ -3,6 +3,7 @@ package com.example.ihonboarding.di
 import android.content.Context
 import com.example.ihonboarding.domain.login.repository.AuthRepository
 import com.example.ihonboarding.domain.login.use_case.LoginUseCase
+import com.example.ihonboarding.domain.login.use_case.RefreshTokenUseCase
 import com.example.ihonboarding.feature.login.data_source.local.SessionManager
 import com.example.ihonboarding.feature.login.data_source.local.SessionManagerImpl
 import com.example.ihonboarding.feature.login.data_source.remote.AuthRemoteDataSource
@@ -14,6 +15,7 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Named
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -34,10 +36,5 @@ class RepositoryModule {
         sessionManager: SessionManager
     ): AuthRepository {
         return AuthRepositoryImpl(authRemoteDataSource, sessionManager)
-    }
-
-    @Provides
-    fun provideLoginUseCase(authRepository: AuthRepository): LoginUseCase {
-        return LoginUseCase(authRepository)
     }
 }
