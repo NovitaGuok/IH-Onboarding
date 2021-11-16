0b78a41d8995c8ee157cc917cd3b7c6baeef2a5f
novitaguok <novitaguok@gmail.com>
2021-11-16 13:48:24 +0700

    Timber planted conditionally

--- app/src/main/java/com/example/ihonboarding/presentation/IhOnboardingApplication.kt
+++ app/src/main/java/com/example/ihonboarding/presentation/IhOnboardingApplication.kt
@@ -1,6 +1,7 @@
 package com.example.ihonboarding.presentation
 
 import android.app.Application
+import com.example.ihonboarding.BuildConfig
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 
@@ -8,6 +9,6 @@ import timber.log.Timber
 class IhOnboardingApplication: Application() {
     override fun onCreate() {
         super.onCreate()
-        Timber.plant(Timber.DebugTree())
+        if(BuildConfig.DEBUG) Timber.plant(Timber.DebugTree())
     }
 }
