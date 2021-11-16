0fb2ed922b536da1bf6bbece9bd5d06746194f83
novitaguok <novitaguok@gmail.com>
2021-11-16 13:22:24 +0700

    Deleted unused files

--- app/src/main/java/com/example/ihonboarding/feature/login/data_source/local/AuthLocalDataSource.kt
+++
@@ -1,8 +0,0 @@
-package com.example.ihonboarding.feature.login.data_source.local
-
-interface AuthLocalDataSource {
-    fun getToken(): String?
-    fun getExpiresAt(): String?
-    fun saveSession(token: String, createdAt: String)
-    fun deleteSession()
-}
--- app/src/main/java/com/example/ihonboarding/feature/login/data_source/remote/api/BaseService.kt
+++
@@ -1,9 +0,0 @@
-package com.example.ihonboarding.feature.login.data_source.remote.api
-
-import okhttp3.ResponseBody
-import retrofit2.http.POST
-
-interface BaseService {
-    @POST("logout")
-    suspend fun logout(): ResponseBody
-}
