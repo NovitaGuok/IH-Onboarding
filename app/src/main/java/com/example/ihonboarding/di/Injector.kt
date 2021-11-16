f70e8dfc81aa1c244d01ca1ed5a234f817c48bbc
novitaguok <novitaguok@gmail.com>
2021-11-16 14:57:33 +0700

    Cleaned folder and files -- specify login only

--- app/src/main/java/com/example/ihonboarding/di/Injector.kt
+++
@@ -1,7 +0,0 @@
-package com.example.ihonboarding.di
-
-import com.example.ihonboarding.di.news.NewsSubcomponent
-
-interface Injector {
-    fun createNewsSubcomponent(): NewsSubcomponent
-}
--- app/src/main/java/com/example/ihonboarding/domain/home/repository/NewsRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.example.ihonboarding.domain.home.repository
-
-import com.example.ihonboarding.data.home.model.News
-
-interface NewsRepository {
-    suspend fun getNews(): List<News>?
-    suspend fun updateNews(): List<News>?
-}
