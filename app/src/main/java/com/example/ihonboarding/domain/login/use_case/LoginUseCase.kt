9f43f88eb6f9b2a2bdabe69780b832c6d740d5d6
novitaguok <novitaguok@gmail.com>
2021-11-16 13:27:21 +0700

    Fixed login use case

--- app/src/main/java/com/example/ihonboarding/domain/login/use_case/LoginUseCase.kt
+++ app/src/main/java/com/example/ihonboarding/domain/login/use_case/LoginUseCase.kt
@@ -1,8 +1,9 @@
 package com.example.ihonboarding.domain.login.use_case
 
 import com.example.ihonboarding.domain.login.repository.AuthRepository
+import com.example.ihonboarding.feature.login.data_source.remote.dto.AuthReqDto
 import javax.inject.Inject
 
 class LoginUseCase @Inject constructor(private val authRepository: AuthRepository) {
-
+    suspend operator fun invoke(authReqDto: AuthReqDto) = authRepository.login(authReqDto)
 }
