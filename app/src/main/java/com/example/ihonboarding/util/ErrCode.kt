9440c24317b2145a2feca79933b8398d89c92184
novitaguok <novitaguok@gmail.com>
2021-11-17 17:15:10 +0700

    Update error code

--- app/src/main/java/com/example/ihonboarding/util/ErrCode.kt
+++ app/src/main/java/com/example/ihonboarding/util/ErrCode.kt
@@ -5,7 +5,7 @@ enum class ErrorCode(val errorCode: Int) {
     CODE_400(400),
     CODE_401(401),
     CODE_422(422),
-    UNKNOWN_ERROR(520);
+    UNKNOWN_ERROR(-1);
 
     companion object {
         fun getErrorCode(errorCode: Int): ErrorCode {
