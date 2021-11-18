e6354facfa6f7f61d89a831b3f694600c0f695b1
novitaguok <novitaguok@gmail.com>
2021-11-18 08:46:08 +0700

    Code fixed

--- app/src/main/java/com/example/ihonboarding/di/RepositoryModule.kt
+++ app/src/main/java/com/example/ihonboarding/di/RepositoryModule.kt
@@ -1,7 +1,6 @@
 package com.example.ihonboarding.di
 
 import com.example.ihonboarding.domain.home.repository.NewsRepository
-import com.example.ihonboarding.domain.home.use_case.GetNewsUseCase
 import com.example.ihonboarding.feature.home.data_source.local.NewsLocalDataSource
 import com.example.ihonboarding.feature.home.data_source.local.NewsLocalDataSourceImpl
 import com.example.ihonboarding.feature.home.data_source.local.dao.NewsDao
@@ -38,10 +37,5 @@ class RepositoryModule {
         newsLocalDataSource: NewsLocalDataSource
     ): NewsRepository {
         return NewsRepositoryImpl(newsRemoteDataSource, newsLocalDataSource)
-    }
-
-    @Provides
-    fun provideGetNewsUseCase(newsRepository: NewsRepository): GetNewsUseCase {
-        return GetNewsUseCase(newsRepository)
     }
 }
